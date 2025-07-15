9e330e340f8d5d2255e38e3baafbca9ee55ce431
hinzer <paul.hinzer@hhi.fraunhofer.de>
2025-07-15 16:24:02 +0200

    add auto select conda to install

--- install.sh
+++ install.sh
@@ -15,7 +15,14 @@ mkdir -p models
 for file in "${files[@]}"; do
     curl -L -o "models/$file" "${base_url}${file}"
 done
-source ~/miniconda3/etc/profile.d/conda.sh
+conda_bin=$(which conda)
+conda_base=$(dirname $(dirname $conda_bin))
+conda_sh="${conda_base}/etc/profile.d/conda.sh"
+if [ ! -f "$conda_sh" ]; then
+    echo "Could not find conda installation"
+    exit 1
+fi
+source "$conda_sh"
 if [ -n "$(conda env list | grep 'ganinv ')" ]; then
     echo "A conda env with the name 'ganinv' already exists"
     exit 1
@@ -23,10 +30,11 @@ fi
 
 conda create -n ganinv python=3.10
 conda activate ganinv
-conda install pytorch==1.13.0 torchvision==0.14.0 torchaudio==0.13.0 pytorch-cuda=11.7 -c pytorch -c nvidia -y
-pip install -r requirements.txt -y
+conda install pytorch==1.13.0 torchvision==0.14.0 torchaudio==0.13.0 pytorch-cuda=11.7 -c pytorch -c nvidia
+pip install -r requirements.txt
 git clone --recursive https://github.com/ashawkey/diff-gaussian-rasterization
 pip install ./diff-gaussian-rasterization
 rm ./diff-gaussian-rasterization
 cd ./preprocess/3DDFA_V2/
 ./build.sh
+
